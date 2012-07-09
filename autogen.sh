512ebbf5dcfb4e438a96568e100c2f0fda751f3f
Lukas Märdian <lukasmaerdian@gmail.com>
2012-07-09 10:50:08 +0200

    initial commit
    Signed-off-by: Lukas Märdian <lukasmaerdian@gmail.com>

---
+++ autogen.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+rm -rf autom4te.cache
+rm -f aclocal.m4
+
+echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS || exit 1
+echo "Running autoconf..." ; autoconf || exit 1
+echo "Running automake..." ; automake --add-missing --copy --gnu || exit 1
+
+if [ -z "$NOCONFIGURE" ]; then
+    ./configure -C "$@"
+fi
---
+++ conf/generic/e/elementary.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export ELM_ENGINE=x11
+export ELM_SCALE=2
+export ELM_FINGER_SIZE=70
